0cdd2a138c401726256d3699d4958927ed5deed4
Eduardo Panneflek <edpanneflek15@gmail.com>
2020-07-13 20:28:35 -0400

    Now Inlcudes 'All' query for all keys

--- Flask/Olympic.py
+++ Flask/Olympic.py
@@ -20,34 +20,41 @@ def home():
 
 # Queries MongoDB
 #/1944/1948/United States/Summer/Aquatics/Gold/
-# http://127.0.0.1:5000/query?Year1=1944&Year2=2014&Country=United%20States&Season=Summer&Sport=Aquatics&Medal=ALL
-
+# http://127.0.0.1:5000/query?Year1=1944&Year2=2014&Country=All&Season=All&Sport=All&Medal=ALL
 @app.route('/query')
 def EventSearch():
     Year1 = request.args.get("Year1")
     Year2 = request.args.get("Year2")
     Country = request.args.get("Country")
     Season = request.args.get("Season")
+    Event = request.args.get("Event")
     Sport = request.args.get("Sport")
     Medal = request.args.get("Medal")
+    
+    MyQuery = {}
 
-    if Season == "Both":
-        Season = ["Summer","Winter"]
+    MyQuery.update({"Year":{"$gte": int(Year1),"$lte": int(Year2)}})
+
+    if Season == "All":
+        MyQuery.update({"Season":{"$in":["Summer","Winter"]}})
     else:
-        Season = [Season, Season]
+        MyQuery.update({"Season":Season})
+
+    if Country != "All":
+        MyQuery.update({"Country":Country})
+                      
+    if Sport != "All":
+        MyQuery.update({"Sport": Sport})
+        
+    if Event != "All":
+        MyQuery.update({"Event": Event})        
 
     if Medal == "All":
-        Medal = ["Gold","Silver","Bronze"]
+        MyQuery.update({"Medal":{"$in":["Gold","Silver","Bronze"]}})
     else:
-        Medal = [Medal, Medal]
-        
-    data = list(db.SportsEventsAndMedals.find({
-        "Season": {"$in": Season}, 
-        "Country": Country,
-        "Sport": Sport,
-        "Year":{"$gte": int(Year1),"$lte": int(Year2)},
-        "Medal" : {"$in": Medal}
-    }))
+        MyQuery.update({"Medal": Medal})
+                    
+    data = list(db.SportsEventsAndMedals.find(MyQuery))
     for item in data:
         del item["_id"]
 
